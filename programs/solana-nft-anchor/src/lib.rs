8b709b05d4ac5de3d4fdb76c2f3993ea27a5fab1
umutsatir <u.satir2021@gtu.edu.tr>
2024-06-30 16:51:16 +0300

    .

--- programs/solana-nft-anchor/src/lib.rs
+++ programs/solana-nft-anchor/src/lib.rs
@@ -13,7 +13,7 @@ use anchor_spl::{
 };
 use mpl_token_metadata::accounts::{ MasterEdition, Metadata as MetadataAccount };
 
-declare_id!("<UPDATE HERE>");
+declare_id!("7fozKV365w5rYjJLv2EATqPBAtaUB7KhH91q3Qfbyzup");
 #[program]
 pub mod solana_nft_anchor {
     use super::*;
